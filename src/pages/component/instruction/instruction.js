f4fe444ef5f95942f2a8697a652d45b6b27fce7b
liulu <2474499153@qq.com>
2020-02-16 22:04:28 +0800

    弹框部分更改

--- src/pages/component/instruction/instruction.js
+++ src/pages/component/instruction/instruction.js
@@ -1,82 +1,147 @@
-import { Button, Descriptions, Tabs } from 'antd';
-import React, { Component } from 'react'; 
-import Link from 'umi/link'
-import InstructionTable from './instructionTable/instructionTable'
-import EditInstructionForm from '../editInstruction/editInstruction'
+import { Button, Descriptions, Tabs, Table, Modal, Input } from 'antd';
+import React, { Component } from 'react';
+import Link from 'umi/link';
+import InstructionTable from './instructionTable/instructionTable';
+import EditInstructionForm from '../editInstruction/editInstruction';
 
-const { TabPane } = Tabs
-const operations = <Button type="primary" onClick={(e) => this.handleClick(e)}>新增接口</Button>
+const { TabPane } = Tabs;
 
 class Instruction extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     labelList:[
-       {
-         name: '接口名称',
-         ename: 'interfaceName',
-         instruction: '接口一'
-       },
-       {
-         name:'接口类型',
-         ename: 'interfaceClass',
-         instruction: 'get'
-       },
-       {
-         name: '创建时间',
-         ename: 'interfaceTime',
-         instruction: '20190102'
-       },
-       {
-         name: '创建人',
-         ename: 'createPeople',
-         instruction: 'solerji'
-       },
-       {
-         name: '接口描述',
-         ename: 'interfaceDetail',
-         instruction: '这是一个好用的接口'
-       }
-     ]
+      labelList: [
+        {
+          name: '接口名称',
+          ename: 'interfaceName',
+          instruction: '接口一',
+        },
+        {
+          name: '接口类型',
+          ename: 'interfaceClass',
+          instruction: 'get',
+        },
+        {
+          name: '创建时间',
+          ename: 'interfaceTime',
+          instruction: '20190102',
+        },
+        {
+          name: '创建人',
+          ename: 'createPeople',
+          instruction: 'solerji',
+        },
+        {
+          name: '接口描述',
+          ename: 'interfaceDetail',
+          instruction: '这是一个好用的接口',
+        },
+      ],
+      visible: false,
     };
-    
   }
 
-  handleClick() {
-    console.log('this is:', this);
-  }
+  hideModal = () => {
+    this.setState({
+      visible: false,
+    });
+  };
 
-  render () {
-      return (
-        <div style={{ 'textAlign': 'start' }}>
+  handleClick = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  render() {
+    const operations = (
+      <Button type="primary" onClick={() => this.handleClick()}>
+        新增接口
+      </Button>
+    );
+
+    const interfaceColumns = [
+      {
+        title: '接口名',
+        dataIndex: 'interfaceName',
+        key: 'interfaceName',
+        render: text => <a>{text}</a>,
+      },
+      {
+        title: '所属分类',
+        dataIndex: 'interfaceClass',
+        key: 'interfaceClass',
+      },
+      {
+        title: '请求类型',
+        dataIndex: 'queryValue',
+        key: 'queryValue',
+      },
+
+      {
+        title: '备注',
+        dataIndex: 'interfaceNote',
+        key: 'interfaceNote',
+      },
+    ];
+
+    const interfaceData = [
+      {
+        interfaceName: 'John Brown',
+        queryValue: 'get',
+        interfaceNote: 'none',
+        interfaceClass: '接口一',
+      },
+    ];
+    return (
+      <div style={{ textAlign: 'start' }}>
         <Tabs tabBarExtraContent={operations}>
-          <TabPane tab="展示" key="1">
-          <Descriptions title="基本信息" size={this.state.size}>
-            {
-              this.state.labelList.map((item, index) => {
-                  return (
-                      <Descriptions.Item 
-                      key={ index }
-                      label= {item.name }>
-                      { item.instruction }
-                  </Descriptions.Item>
-                  )
-              })
-            } 
-          </Descriptions>
-          <InstructionTable></InstructionTable>
+          <TabPane tab="全部接口" key="1">
+            <Table
+              columns={interfaceColumns}
+              dataSource={interfaceData}
+              pagination={false}
+              size="middle"
+            ></Table>
           </TabPane>
-          <TabPane tab="编辑" key="2">
+          <TabPane tab="展示" key="2">
+            <Descriptions title="基本信息" size={this.state.size}>
+              {this.state.labelList.map((item, index) => {
+                return (
+                  <Descriptions.Item key={index} label={item.name}>
+                    {item.instruction}
+                  </Descriptions.Item>
+                );
+              })}
+            </Descriptions>
+            <InstructionTable></InstructionTable>
+          </TabPane>
+          <TabPane tab="编辑" key="3">
             <EditInstructionForm></EditInstructionForm>
           </TabPane>
         </Tabs>
+        <div></div>
         <div>
-        </div>
-        <div>
-        <Link to="/interface">进入接口测试页面</Link>
+          <Link to="/interface">进入接口测试页面</Link>
+          <Modal
+            title="新增接口"
+            visible={this.state.visible}
+            onOk={this.hideModal}
+            onCancel={this.hideModal}
+            okText="确认"
+            cancelText="取消"
+          >
+            <Input></Input>
+            <br />
+            <br />
+            <Input></Input>
+            <br />
+            <br />
+            <Input></Input>
+          </Modal>
         </div>
       </div>
-      )
+    );
   }
 }
 
--- src/pages/component/interfaceList/interfaceList.js
+++ src/pages/component/interfaceList/interfaceList.js
@@ -1,37 +1,48 @@
 import { List, Button } from 'antd';
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 
 class InterfaceList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       listNames: [
         {
-          name: '接口一'
+          name: '接口一',
         },
         {
-          name: '接口二'
+          name: '接口二',
         },
         {
-          name: '接口三'
-        }
-      ]
-    }
+          name: '接口三',
+        },
+      ],
+    };
   }
-  render () {
-      return (
-        <div>
+
+  editItem() {}
+
+  delItem() {}
+
+  render() {
+    return (
+      <div>
         <List
           header={<div>所有分类</div>}
           bordered
           dataSource={this.state.listNames}
           renderItem={item => (
-            <List.Item actions={[<Button key="list-loadmore-edit">新增</Button>, <Button key="list-loadmore-more">删除</Button>]}>
-            {item.name}
+            <List.Item
+              actions={[
+                <Button onClick={() => this.editItem()}>编辑</Button>,
+                <Button onClick={() => this.delItem()}>删除</Button>,
+              ]}
+            >
+              {item.name}
             </List.Item>
-      )}/>
+          )}
+        />
       </div>
-      )
+    );
   }
 }
 
